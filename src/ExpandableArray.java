ca37655ddd356c71ad88b3d806d9651a1fb0032b
Eran9 <eran.perera2010@gmail.com>
2024-06-11 00:41:41 +0530

    Finished ExpandableArray method

--- src/ExpandableArray.java
+++ src/ExpandableArray.java
@@ -1,2 +1,40 @@
+import java.util.Arrays;
+
 public class ExpandableArray {
+    String[] arr = new String[1];
+    int currentIndex = 0;
+    int newSize = 1;
+    String[] newArr;
+    int length;
+
+    public void add(String value) {
+        //
+        if (currentIndex < arr.length) {
+            arr[currentIndex] = value;
+            this.currentIndex += 1;
+        } else {
+            newSize += 1;
+            newArr = new String[newSize]; // instantiate a larger array
+            System.arraycopy(arr, 0, newArr, 0, arr.length); // copy elements in old array to new array
+            arr = newArr; // reference old array to new array
+            newArr[currentIndex] = value;
+            this.currentIndex += 1;
+            this.length = newArr.length;
+        }
+    }
+
+    public void printEach(){
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+
+    public String toString(){
+        //System.out.println(Arrays.toString(arr));
+        return Arrays.toString(arr);
+    }
+
+    public String index(int index){
+        return arr[index];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExpandableArray array = new ExpandableArray();
+        array.add("Eran");
+        array.add("Timothy");
+        array.add("Perera");
+        array.add("is");
+        array.add("me😊");
+        System.out.println(array.index(0) + " " + array.index(1) + " " + array.index(2));
+        array.printEach();
+        System.out.println(array.toString());
+
     }
 }
